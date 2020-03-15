cd220bfcbd835120d3bf9f36077e2b66ed0c1128
Kathryn Clark <kathrynlael@gmail.com>
2020-03-15 21:30:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array, starting_point = 0)
-  source_array.reduce(starting_point){|sum, num| ()
+  source_array.reduce(starting_point){|sum, num| (sum + num)}
 end
 
 # My Code here....
