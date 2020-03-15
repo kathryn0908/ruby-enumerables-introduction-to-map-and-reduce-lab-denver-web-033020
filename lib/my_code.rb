cf9d95ea0ec5ef4fb379666967419af30b1f20ea
Kathryn Clark <kathrynlael@gmail.com>
2020-03-15 21:31:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,4 +18,8 @@ def reduce_to_total(source_array, starting_point = 0)
   source_array.reduce(starting_point){|sum, num| (sum + num)}
 end
 
+def reduce_to_all_true(source_array)
+  source_array.reduce{|i| (i == true)}
+end
+
 # My Code here....
