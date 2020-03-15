574606c8fb0f92c9ca849f29054446b7fcc4b4fc
Kathryn Clark <kathrynlael@gmail.com>
2020-03-15 21:28:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,4 +6,15 @@ def map_to_no_change(source_array)
   source_array.map {|i| i}
 end
 
+def map_to_double(source_array)
+  source_array.map{|i| (i * 2)} 
+end 
+
+def map_to_square(source_array)
+  source_array.map{|i| (i ** 2)}
+end 
+
+def reduce_to_total(source_array, starting_point)
+end
+
 # My Code here....
