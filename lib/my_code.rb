2b114afc1c3e074a35c69974902d3011b2d04b34
Kathryn Clark <kathrynlael@gmail.com>
2020-03-15 21:40:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,11 +27,10 @@ def reduce_to_any_true(source_array)
   while i < source_array.length do 
     if source_array[i] == true
       return true 
-    else 
-      return false 
     end
     i +=1 
   end 
+  false
 end
 
 # My Code here....
